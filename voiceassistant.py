a4d92959e202c57a3b9a6fba616351936e9edfd3
JRDMIII <112983868+JRDMIII@users.noreply.github.com>
2022-09-06 22:40:51 +0100

    Voice Assistant V3.1
    In this version of the voice assistant the command check has been separated from the getAudio() command for better troubleshooting in the event of errors. More possible commands have been added to the voice assistant's capabilities. The exception statement has also been simplified to just counter any possible errors that may come from recognising audio

--- voiceassistant.py
+++ voiceassistant.py
@@ -10,6 +10,20 @@ import pyttsx3
 # This module is required for the microphone
 import pyaudio
 
+def commandCheck(audioInput):
+    # This list of if statements looks for keywords within the audio being received to decide the correct output
+    if "hello" in audioInput:
+        assistantVoice("i think you want me to play something?")
+
+    if "play" in audioInput:
+        assistantVoice("i think you want me to play something?")
+
+    if "code" in audioInput:
+        assistantVoice("i think you want me to code something?")
+
+    if "homework" in audioInput:
+        assistantVoice("Do you want me to show you your homework?")
+
 # This subroutine will produce the audio file which the voice assistant will play back
 num = 1
 def assistantVoice(output):
@@ -22,29 +36,26 @@ def assistantVoice(output):
     engine.say(output)
     engine.runAndWait()
 
-# This instantiates an object for recognising voices
-rec = sr.Recognizer()
-m = sr.Microphone()
 
+# This subroutine is used to
 def getAudio():
+    # This instantiates an object for recognising voices
+    rec = sr.Recognizer()
+    m = sr.Microphone()
+
     with m as source:
 
-        audio = rec.listen(source)
+        # This begins recording audio coming from our mic source
+        print("Speak...")
+        audio = rec.listen(source, phrase_time_limit=7)
+        print("Stop.")
 
         try:
-            text = rec.recognize_google(audio)
-            print(text)
-        except sr.UnknownValueError:
+            text = rec.recognize_google(audio, language='en-US')
+            print("You : ", text)
+            return text
+        except:
             print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not get results from GSR service; {0}".format(e))
 
-        # This list of if statements looks for keywords within the audio being received to decide the correct output
-        if "play" in text:
-            assistantVoice("i think you want me to play something?")
-
-        if "code" in text:
-            assistantVoice("i think you want me to code something?")
-
-        if "homework" in text:
-            assistantVoice("Do you want me to show you your homework?")
+while True:
+    getAudio()
